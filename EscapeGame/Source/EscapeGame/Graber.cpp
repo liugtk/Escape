85e6ee7e05f94754c6fdeb4225789a90d26938ff
liugtk <gtk.carter.liu@gmail.com>
2017-08-06 23:43:50 +0800

    79

--- EscapeGame/Source/EscapeGame/Graber.cpp
+++ EscapeGame/Source/EscapeGame/Graber.cpp
@@ -5,6 +5,9 @@
 #include "GameFramework/Controller.h"
 #include "GameFramework/PlayerController.h"
 
+#include "DrawDebugHelpers.h"
+//#include "line"
+
 #define OUT
 
 // Sets default values for this component's properties
@@ -40,9 +43,46 @@ void UGraber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponen
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
 	OUT	PlayerViewPointLocation, 
 	OUT	PlayerViewPointRotation);
+	
 	//ray-cast out to reach distance
 	//see what we hit
-	UE_LOG(LogTemp, Warning, TEXT("PlayerViewPointLocation Report: %s"), *PlayerViewPointLocation.ToString());
-	UE_LOG(LogTemp, Warning, TEXT("PlayerViewPointRotation Report: %s"), *PlayerViewPointRotation.ToString());
+	//UE_LOG(LogTemp, Warning, TEXT("PlayerViewPointLocation Report: %s"), *PlayerViewPointLocation.ToString());
+	//UE_LOG(LogTemp, Warning, TEXT("PlayerViewPointRotation Report: %s"), *PlayerViewPointRotation.ToString());
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+		
+	// draw 
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255,0,0),
+		false,
+		0.f,
+		0,
+		10.f
+	);
+
+	//set up query parameters.
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+	// line-trace (AKA rat-cast) out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+
+	//See what we hit
+	AActor* ActorHit =  Hit.GetActor();
+
+	if (ActorHit) {
+		UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"), *(ActorHit->GetName()));
+	}
+	
+
+
+
 }
 
